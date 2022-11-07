dc1f0a2f22ab6952c503ea23e9b532283f1d0893
durgaselvans89 <42055866+durgaselvans89@users.noreply.github.com>
2022-11-07 18:00:55 +0530

    Create config.php

---
+++ app/api/config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('HOST', 'localhost');
+define('USER_NAME', 'root');
+define('PASSWORD', '');
+define('DB_NAME', 'laravel_room_allocation');
+define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'].'/room_booking/app/');
+
+?>
